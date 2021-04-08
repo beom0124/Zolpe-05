60f95f9761aeebaf01b66722fe68bffa03b357d4
hansung <beom000124@gmail.com>
2021-04-08 15:16:45 +0900

    bottomnav

--- app/src/main/java/com/example/zolpe_05/MainActivity.kt
+++ app/src/main/java/com/example/zolpe_05/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.zolpe_05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*var bottomnavigationview = BottomNavigationView
+        bottomnavigationview.setOnNavigationItemSelectedListener(this)*/
+
+        val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                    R.id.home -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.linearLayout , HomeFragment()).commitAllowingStateLoss()
+                        return true
+                    }
+                    R.id.camera -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.linearLayout, TVFragment()).commitAllowingStateLoss()
+                        return true
+                    }
+                    R.id.chat -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.linearLayout, CalendarFragment()).commitAllowingStateLoss()
+                        return true
+                    }
+                }
+
+            }
+            false
+        }
+
+        supportFragmentManager.beginTransaction().add(R.id.linearLayout, HomeFragment()).commit()
     }
-}
+ /*override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.home -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linearLayout , HomeFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.camera -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linearLayout, TVFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.chat -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linearLayout, CalendarFragment()).commitAllowingStateLoss()
+                return true
+            }
+        }*/
+
+        return false
+    }
