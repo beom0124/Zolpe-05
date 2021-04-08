bea60f1254d50d34c5cf4e42c18220e10cbb2e47
hansung <beom000124@gmail.com>
2021-04-08 20:58:45 +0900

    bottomnav, actionbar delete

--- app/src/main/java/com/example/zolpe_05/CameraActivity.kt
+++ app/src/main/java/com/example/zolpe_05/CameraActivity.kt
@@ -2,17 +2,17 @@ package com.example.zolpe_05
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class CameraActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_camera)
         var actionBar : ActionBar?
 
         actionBar = supportActionBar
@@ -24,28 +24,27 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-
         when(item.itemId) {
             R.id.home -> {
-                Toast.makeText(this,"이미 메인 메뉴에 있습니다.", Toast.LENGTH_SHORT).show()
+                val homeIntent = Intent(this, MainActivity::class.java)
+                homeIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(homeIntent)
+                finish()
+                //인텐트 flag랑 finish 해도 안 넘어계속 중간에 메인엑티비티 나옴
                 return true
             }
             R.id.camera -> {
-                val cameraIntent = Intent(this, CameraActivity::class.java)
-                cameraIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                startActivity(cameraIntent)
-                finish()
+                Toast.makeText(this,"이미 카메라 메뉴에 있습니다.", Toast.LENGTH_SHORT).show()
                 return true
             }
             R.id.chat -> {
-                val chatIntent = Intent(this, ChatActivity::class.java)
+                val chatIntent = Intent(this, MainActivity::class.java)
                 chatIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(chatIntent)
                 finish()
                 return true
             }
         }
-
         return false
     }
 }
--- app/src/main/java/com/example/zolpe_05/ChatActivity.kt
+++ app/src/main/java/com/example/zolpe_05/ChatActivity.kt
@@ -8,11 +8,10 @@ import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class ChatActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_camera)
         var actionBar : ActionBar?
 
         actionBar = supportActionBar
@@ -24,28 +23,26 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-
         when(item.itemId) {
             R.id.home -> {
-                Toast.makeText(this,"이미 메인 메뉴에 있습니다.", Toast.LENGTH_SHORT).show()
+                val homeIntent = Intent(this, MainActivity::class.java)
+                homeIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(homeIntent)
+                finish()
                 return true
             }
             R.id.camera -> {
-                val cameraIntent = Intent(this, CameraActivity::class.java)
+                val cameraIntent = Intent(this, MainActivity::class.java)
                 cameraIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(cameraIntent)
                 finish()
                 return true
             }
             R.id.chat -> {
-                val chatIntent = Intent(this, ChatActivity::class.java)
-                chatIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                startActivity(chatIntent)
-                finish()
+                Toast.makeText(this,"이미 챗봇 메뉴에 있습니다.", Toast.LENGTH_SHORT).show()
                 return true
             }
         }
-
         return false
     }
 }
