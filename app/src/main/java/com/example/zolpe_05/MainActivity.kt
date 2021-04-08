3f92cd3d6c13b522cfd300ed59231b480158478f
hansung <beom000124@gmail.com>
2021-04-08 15:48:54 +0900

    bottomnav

--- app/src/main/java/com/example/zolpe_05/MainActivity.kt
+++ app/src/main/java/com/example/zolpe_05/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.zolpe_05
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -10,47 +13,38 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*var bottomnavigationview = BottomNavigationView
-        bottomnavigationview.setOnNavigationItemSelectedListener(this)*/
+        var actionBar : ActionBar?
 
-        val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                    R.id.home -> {
-                        supportFragmentManager.beginTransaction().replace(R.id.linearLayout , HomeFragment()).commitAllowingStateLoss()
-                        return true
-                    }
-                    R.id.camera -> {
-                        supportFragmentManager.beginTransaction().replace(R.id.linearLayout, TVFragment()).commitAllowingStateLoss()
-                        return true
-                    }
-                    R.id.chat -> {
-                        supportFragmentManager.beginTransaction().replace(R.id.linearLayout, CalendarFragment()).commitAllowingStateLoss()
-                        return true
-                    }
-                }
+        actionBar = supportActionBar
+        actionBar?.hide()
 
-            }
-            false
-        }
+        var bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
-        supportFragmentManager.beginTransaction().add(R.id.linearLayout, HomeFragment()).commit()
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+
+        //supportFragmentManager.beginTransaction().add(R.id.linearLayout, HomeFragment()).commit()
     }
- /*override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
         when(item.itemId) {
             R.id.home -> {
-                supportFragmentManager.beginTransaction().replace(R.id.linearLayout , HomeFragment()).commitAllowingStateLoss()
+                Toast.makeText(this,"홈",Toast.LENGTH_SHORT).show()
+                //supportFragmentManager.beginTransaction().replace(R.id.linearLayout , HomeFragment()).commitAllowingStateLoss()
                 return true
             }
             R.id.camera -> {
-                supportFragmentManager.beginTransaction().replace(R.id.linearLayout, TVFragment()).commitAllowingStateLoss()
+                Toast.makeText(this,"카메라",Toast.LENGTH_SHORT).show()
+                //supportFragmentManager.beginTransaction().replace(R.id.linearLayout, TVFragment()).commitAllowingStateLoss()
                 return true
             }
             R.id.chat -> {
-                supportFragmentManager.beginTransaction().replace(R.id.linearLayout, CalendarFragment()).commitAllowingStateLoss()
+                Toast.makeText(this,"챗봇",Toast.LENGTH_SHORT).show()
+                //supportFragmentManager.beginTransaction().replace(R.id.linearLayout, CalendarFragment()).commitAllowingStateLoss()
                 return true
             }
-        }*/
+        }
 
         return false
     }
+}
