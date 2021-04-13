e321bbe0a862f9b705f3449649de2bb5d031e345
hansung <beom000124@gmail.com>
2021-04-13 18:43:26 +0900

    bae

--- app/src/main/java/com/example/zolpe_05/LoginActivity.kt
+++ app/src/main/java/com/example/zolpe_05/LoginActivity.kt
@@ -1,4 +1,24 @@
 package com.example.zolpe_05
 
-class LoginActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            if(height_insert.isNullOrEmpty() && weight_insert.isNullOrEmpty() && topsize_insert.isNullEmpty() && undersize_insert.isNullOrEmpty() && google_id_insert.isNullEmpty() && google_pw_insert.isNullEmpty()){
+                Toast.makeText(this, "정보를 입력해주세요", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
