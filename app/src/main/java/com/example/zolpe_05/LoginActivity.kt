d9488b8d73399f0ec44350b3da0b22b87b3e503d
hansung <beom000124@gmail.com>
2021-04-13 19:12:46 +0900

    bae

--- app/src/main/java/com/example/zolpe_05/LoginActivity.kt
+++ app/src/main/java/com/example/zolpe_05/LoginActivity.kt
@@ -2,22 +2,24 @@ package com.example.zolpe_05
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        var actionBar : ActionBar?
 
-        buttonLogin.setOnClickListener {
+        actionBar = supportActionBar
+        actionBar?.hide()
+
+        buttonLogin.setOnClickListener { //버튼 왜 안됨?
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             if(height_insert.isNullOrEmpty() && weight_insert.isNullOrEmpty() && topsize_insert.isNullEmpty() && undersize_insert.isNullOrEmpty() && google_id_insert.isNullEmpty() && google_pw_insert.isNullEmpty()){
-                Toast.makeText(this, "정보를 입력해주세요", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "빈칸 없이 정보를 입력해주세요", Toast.LENGTH_SHORT).show()
             }
         }
     }
