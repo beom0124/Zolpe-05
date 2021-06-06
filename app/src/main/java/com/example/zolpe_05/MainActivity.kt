120301d88eb23335486149a26fd58d583590ba2f
beom0124 <beom000124@gmail.com>
2021-06-06 21:29:07 +0900

    날씨 메시지, 아이콘 표시 완성

--- app/src/main/java/com/example/zolpe_05/MainActivity.kt
+++ app/src/main/java/com/example/zolpe_05/MainActivity.kt
@@ -9,19 +9,21 @@ import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.zolpe_05.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+//import com.google.firebase.firestore.FirebaseFirestore
+//import com.google.firebase.firestore.ktx.firestore
+//import com.google.firebase.ktx.Firebase
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 val num_of_rows = 10
 val page_no = 1
 val data_type = "JSON"
-val base_time = 2300
-val base_date = 20210605
+val base_time = 2000
+val base_date = 20210606
 val nx = "60"
 val ny = "127" //성북구 삼선동 좌표임
 
@@ -46,7 +48,7 @@ data class ITEMS(
 )
 data class ITEM(
     val category: String,
-   val fcstValue: String
+    val fcstValue: String
 )
 
 interface  WeatherInterface{
@@ -82,6 +84,9 @@ var weatherText: String = ""
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
+//    private val db: FirebaseFirestore = Firebase.firestore
+//    private val itemsCollectionRef = db.collection("Coat")
+
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
     override  fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -94,19 +99,15 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         var bottomNavigationView = binding.bottomNavigationView
         bottomNavigationView.setOnNavigationItemSelectedListener(this)
 
-        val rightarrow = binding.rightArrow
-        rightarrow.setOnClickListener {
-            Toast.makeText(this, "right arrow", Toast.LENGTH_SHORT).show()
-        }
 
         val call = ApiObject.retrofitService.GetWeather(data_type, num_of_rows, page_no, base_date, base_time, nx, ny)
         call.enqueue(object : retrofit2.Callback<WEATHER>{
             override fun onResponse(call: Call<WEATHER>, response: Response<WEATHER>) {
                 if(response.isSuccessful){
-                    Log.d("weather",response.body()!!.response.body.items.toString())
+                    Log.d("weather",response.body()!!.response.body.items.toString()) //날짜, 시간 바꾸면 여기서 뻗음 이유 찾아야해
                     weatherResult =response.body()!!.response.body.items.toString().split("(")
                     setWeatherInfo()
-                    setWeatherText()
+                    setWeatherTextIcon()
                 }
             }
             override fun onFailure(call: Call<WEATHER>, t: Throwable) {
@@ -115,11 +116,41 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         })
     }
 
+    fun setClothInfo(){
+        var names = emptyList<String>()
+        var prices = emptyList<String>()
+         var imgs = emptyList<String>()
+
+
+        if (temp <= 5){
+            if(rainStatus>=1){ //비가오면
+
+            }
+
+            //itemsCollectionRef = db.collection("Coat")
+        }
+        else if(temp in 5..15){
+            if(rainStatus>=1){
+                //트랜치
+            }
+
+        }
+        else if(temp in 16..25){
+
+        }
+        else if(temp >=26){
+
+        }
+        else{
+            Log.d("tempCloth","temp error!")
+        }
+    }
+
     fun setWeatherInfo(){
         var rainPercentInfo = weatherResult[2]
         var rainStatusInfo = weatherResult[3]
-        var skyStatusInfo = weatherResult[5]
-        var tempInfo = weatherResult[6]
+        var skyStatusInfo = weatherResult[7] //얘랑 날씨가 5,6값 줘야할때도 있음 왔다갔다혀
+        var tempInfo = weatherResult[8]
         var find1 = rainPercentInfo.lastIndexOf("=")
         var find2 = rainPercentInfo.indexOf(")")
         val find3 = rainStatusInfo.lastIndexOf("=")
@@ -134,14 +165,14 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         temp = tempInfo.slice(IntRange(find7+1,find8-1)).toInt()
     }
 
-    fun setWeatherText(){
+    fun setWeatherTextIcon(){
         var rainPercentText: String = ""
         var rainStatusText: String = ""
         var skyStatusText: String = ""
         var tempText: String = ""
 
         when (rainStatus){
-            0-> rainStatusText  = ""
+            0-> rainStatusText  = "비가 내릴 가능성이 있고"
             1-> rainStatusText = "비가 내리겠고"
             2-> rainStatusText = "진눈개비가 날리겠고"
             3-> rainStatusText = "눈이 내리겠고"
@@ -151,21 +182,55 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             7-> rainStatusText = "눈이 날리겠고"
             else -> rainStatusText = "???"
         }
-
         if(rainPercent ==0){
             rainPercentText = "오늘은 비 소식이 없고"
         }
         else{
             rainPercentText = "오늘은 "+ rainPercent+"%의 확률로 " + rainStatusText
         }
-
         when(skyStatus){
             1-> skyStatusText = "맑은 날씨입니다."
             3-> skyStatusText = "구름 많은 날씨입니다."
             4-> skyStatusText = "흐린 날씨입니다."
             else -> skyStatusText = "???"
         }
-        tempText = temp.toString() + "°C의 " //온도에 따른 조건문 아직 구현 안 함
+        tempText = temp.toString() + "°C의 "
+
+        if(skyStatus == 1){
+            if(rainStatus ==0){
+                binding.weatherImageView.setImageResource(R.drawable.sun)
+            }
+            else if(rainStatus == 1 || rainStatus == 4 || rainStatus == 5){
+                binding.weatherImageView.setImageResource(R.drawable.suncloudrain)
+            }
+            else if(rainStatus ==2 || rainStatus == 6 || rainStatus == 7){
+                binding.weatherImageView.setImageResource(R.drawable.suncloudsnow)
+            }
+        }
+
+        if(skyStatus == 2){
+            if(rainStatus ==0){
+                binding.weatherImageView.setImageResource(R.drawable.suncloud)
+            }
+            else if(rainStatus == 1 || rainStatus == 4 || rainStatus == 5){
+                binding.weatherImageView.setImageResource(R.drawable.suncloudrain)
+            }
+            else if(rainStatus ==2 || rainStatus == 6 || rainStatus == 7){
+                binding.weatherImageView.setImageResource(R.drawable.suncloudsnow)
+            }
+        }
+
+        if(skyStatus == 3){
+            if(rainStatus ==0){
+                binding.weatherImageView.setImageResource(R.drawable.cloud)
+            }
+            else if(rainStatus == 1 || rainStatus == 4 || rainStatus == 5){
+                binding.weatherImageView.setImageResource(R.drawable.cloudrain)
+            }
+            else if(rainStatus ==2 || rainStatus == 6 || rainStatus == 7){
+                binding.weatherImageView.setImageResource(R.drawable.cloudsnow)
+            }
+        }
 
         weatherText = rainPercentText + "\n"+ tempText + skyStatusText
         binding.weatherMessage.setText(weatherText)
