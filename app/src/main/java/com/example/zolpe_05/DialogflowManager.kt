53fb76caa0713d6e6471c8de8e40e3283a453534
beom0124 <beom000124@gmail.com>
2021-06-16 05:36:33 +0900

    구글 로그인 및 회원가입 firebase 연동 성공
    다이얼로그플로우 관련 해서 다 주석처리해놓음

--- app/src/main/java/com/example/zolpe_05/DialogflowManager.kt
+++ app/src/main/java/com/example/zolpe_05/DialogflowManager.kt
@@ -1,67 +1,67 @@
-package com.example.zolpe_05
-
-import android.content.res.Resources
-import com.google.api.Context
-import com.google.api.gax.core.FixedCredentialsProvider
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.auth.oauth2.ServiceAccountCredentials
-import com.google.cloud.dialogflow.v2.*
-import com.google.protobuf.Struct
-import com.google.protobuf.Value
-import java.io.InputStream
-import java.util.*
+//package com.example.zolpe_05
 //
-
-class DialogflowManager {
-    private var client: SessionsClient? = null
-    private var session: SessionName? = null
-    private val LANGUAGE_CODE = "ko"
-
-    fun initAssistant(context: Context) {
-        try {
-            val resources = android.content.res.Resources.getSystem()
-            val stream = resources.openRawResource(R.raw.credentials)
-            val credentials = GoogleCredentials.fromStream(stream)
-            val projectId = (credentials as ServiceAccountCredentials).projectId
-            val uuid = UUID.randomUUID().toString() //uuid import 맞게했나 아리까리
-            val settingsBuilder = SessionsSettings.newBuilder()
-            val sessionsSettings =
-                settingsBuilder.setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build()
-
-
-            this.client = SessionsClient.create(sessionsSettings)
-            this.session = SessionName.of(projectId, uuid)
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    @Throws(Exception::class)
-    fun detectIntentText(somePayload: Int, text: String): QueryResult {
-
-        if(client == null || session == null){
-            throw Exception("Error: no dialogflow client")
-        }
-        // Set the text (input) and language code (en) for the query
-        val textInput = TextInput.newBuilder().setText(text).setLanguageCode(LANGUAGE_CODE)
-        // Build the query with the TextInput
-        val queryInput = QueryInput.newBuilder().setText(textInput).build()
-        //Save received payload into a protobuf Struct
-        val somePayload = Value.newBuilder().setNumberValue(somePayload.toDouble()).build()
-        val struct = Struct.newBuilder()
-        struct.putFields("somePayload", somePayload)
-        //Set queryParameters
-        val queryParameters = QueryParameters.newBuilder().setPayload(struct).build()
-        //Build the request
-        val request = DetectIntentRequest.newBuilder()
-            .setSession(session.toString())
-            .setQueryInput(queryInput)
-            .setQueryParams(queryParameters)
-            .build()
-        // Performs the detect intent request
-        val response = client!!.detectIntent(request)
-        // Display the query result
-        return response.queryResult
-    }
-}
+//import android.content.res.Resources
+//import com.google.api.Context
+//import com.google.api.gax.core.FixedCredentialsProvider
+//import com.google.auth.oauth2.GoogleCredentials
+//import com.google.auth.oauth2.ServiceAccountCredentials
+//import com.google.cloud.dialogflow.v2.*
+//import com.google.protobuf.Struct
+//import com.google.protobuf.Value
+//import java.io.InputStream
+//import java.util.*
+////
+//
+//class DialogflowManager {
+//    private var client: SessionsClient? = null
+//    private var session: SessionName? = null
+//    private val LANGUAGE_CODE = "ko"
+//
+//    fun initAssistant(context: Context) {
+//        try {
+//            val resources = android.content.res.Resources.getSystem()
+//            val stream = resources.openRawResource(R.raw.credentials)
+//            val credentials = GoogleCredentials.fromStream(stream)
+//            val projectId = (credentials as ServiceAccountCredentials).projectId
+//            val uuid = UUID.randomUUID().toString() //uuid import 맞게했나 아리까리
+//            val settingsBuilder = SessionsSettings.newBuilder()
+//            val sessionsSettings =
+//                settingsBuilder.setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build()
+//
+//
+//            this.client = SessionsClient.create(sessionsSettings)
+//            this.session = SessionName.of(projectId, uuid)
+//
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
+//    }
+//
+//    @Throws(Exception::class)
+//    fun detectIntentText(somePayload: Int, text: String): QueryResult {
+//
+//        if(client == null || session == null){
+//            throw Exception("Error: no dialogflow client")
+//        }
+//        // Set the text (input) and language code (en) for the query
+//        val textInput = TextInput.newBuilder().setText(text).setLanguageCode(LANGUAGE_CODE)
+//        // Build the query with the TextInput
+//        val queryInput = QueryInput.newBuilder().setText(textInput).build()
+//        //Save received payload into a protobuf Struct
+//        val somePayload = Value.newBuilder().setNumberValue(somePayload.toDouble()).build()
+//        val struct = Struct.newBuilder()
+//        struct.putFields("somePayload", somePayload)
+//        //Set queryParameters
+//        val queryParameters = QueryParameters.newBuilder().setPayload(struct).build()
+//        //Build the request
+//        val request = DetectIntentRequest.newBuilder()
+//            .setSession(session.toString())
+//            .setQueryInput(queryInput)
+//            .setQueryParams(queryParameters)
+//            .build()
+//        // Performs the detect intent request
+//        val response = client!!.detectIntent(request)
+//        // Display the query result
+//        return response.queryResult
+//    }
+//}
---
+++ app/src/main/java/com/example/zolpe_05/SignUpActivity.kt
@@ -0,0 +1,89 @@
+package com.example.zolpe_05
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+class SignUpActivitiy : AppCompatActivity() {
+    //private val mAuth = FirebaseAuth.getInstance() //파이어베이스 인증 연동 ( 현서 11/1일 )
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+        findViewById<View>(R.id.member_button).setOnClickListener(onClickListener) // 회원가입 버튼 클릭시 메소드 실행 ( 현서 11/1일 )
+        auth = Firebase.auth
+    }
+
+    //클릭 이벤트 Switch 문으로 넘겨진 id 값에 따라 메소드 처리 ( 현서 11/1일 )
+    var onClickListener =
+        View.OnClickListener { v ->
+            when (v.id) {
+                R.id.member_button -> singUp()
+            }
+        }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        //
+        updateUI(currentUser)
+    }
+
+    private fun singUp() {
+
+        val email = (findViewById<View>(R.id.signup_email) as EditText).text.toString()
+        val password = (findViewById<View>(R.id.signup_password) as EditText).text.toString()
+        val passwordCheck =
+            (findViewById<View>(R.id.signup_passconfirm) as EditText).text.toString()
+        if (email.length > 0 && password.length > 0 && passwordCheck.length > 0) //null 값 체크 ( 현서 11/1일 )
+        {
+            // 패스워드 같을 때만 회원가입 완료되게 ( 현서 11/1일 )
+            if (password == passwordCheck) {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(
+                        this
+                    ) { task ->
+                        if (task.isSuccessful) {
+                            // 회원가입 성공시 ( 현서 11/1일 )
+                            val user = auth.currentUser
+                            Toast.makeText(
+                                this@SignUpActivitiy,
+                                "회원가입이 성공했습니다.",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                            val intent = Intent(this@SignUpActivitiy, LoginActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            //회원가입 실패시 ( 현서 11/1일 )
+                            Toast.makeText(
+                                this@SignUpActivitiy,
+                                "회원가입이 실패했습니다.",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this@SignUpActivitiy, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this@SignUpActivitiy, "이메일또는 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+    companion object {
+        private const val TAG = "SignUpActivity"
+    }
+}
