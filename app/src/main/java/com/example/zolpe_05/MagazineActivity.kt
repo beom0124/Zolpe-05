f9f806912adc28e64fc37a8f6154df75720f129c
hansung <beom000124@gmail.com>
2021-06-16 11:56:15 +0900

    매거진 완성

--- app/src/main/java/com/example/zolpe_05/MagazineActivity.kt
+++ app/src/main/java/com/example/zolpe_05/MagazineActivity.kt
@@ -11,10 +11,16 @@ import androidx.appcompat.app.ActionBar
 import com.example.zolpe_05.databinding.ActivityMagazineBinding
 import com.example.zolpe_05.ui.ChatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_magazine.view.*
+import java.util.*
 
 class MagazineActivity : AppCompatActivity() , BottomNavigationView.OnNavigationItemSelectedListener {
     val binding by lazy { ActivityMagazineBinding.inflate(layoutInflater) }
+    private val db: FirebaseFirestore = Firebase.firestore
+    var itemsCollectionRef = db.collection("Magazine")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -26,14 +32,25 @@ class MagazineActivity : AppCompatActivity() , BottomNavigationView.OnNavigation
         var bottomNavigationView = binding.bottomNavigationView
         bottomNavigationView.setOnNavigationItemSelectedListener(this)
 
+        selectMagazine()
+
+    }
+
+    fun selectMagazine(){
         binding.webView.settings.javaScriptEnabled = true // 자바 스크립트 허용
 
-        // 웹뷰안에 새 창이 뜨지 않도록 방지
         binding.webView.webViewClient = WebViewClient()
         binding.webView.webChromeClient = WebChromeClient()
 
-        // 원하는 주소를 WebView에 연결
-        binding.webView.loadUrl("https://naver.com")
+
+        val random = Random()
+        val magazine_random_num = random.nextInt(49)+1
+        itemsCollectionRef.document(magazine_random_num.toString()).get()
+            .addOnSuccessListener {
+                val link_temp = it["link"].toString()
+                val link_final = "https:"+link_temp
+                binding.webView.loadUrl(link_final)
+            }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
